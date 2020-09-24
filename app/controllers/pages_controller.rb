33ef0b4c4aab9dde33ed6a04f8bb582d73a701a2
joaoamaro <jamarodias@gmail.com>
2020-09-24 00:18:15 -0300

    parcial do exercício

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,19 @@
 class PagesController < ApplicationController
     def about
     end
+
+    def contact
+        @members = [ 'thanh', 'dimitri', 'germain', 'damien', 'julien' ]
+
+        if params[:member]
+        @members = @members.select { |member| member.start_with?(params[:member]) }
+        end
+    end
+
+    def home
+    end
+
+    def index
+        @members2 = [ 'dhanh', 'gimitri', 'hermain', 'jamien', 'hulien' ]
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'pages#home'
   get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
+  get 'home', to: 'pages#home'
+  get 'index', to: 'pages#index'
 end
